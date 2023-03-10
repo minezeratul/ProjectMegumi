f122c3f320df0190b70c4692d9012108ec1d01c9
minezeratul <619492300@qq.com>
2023-03-10 21:56:24 +0800

    my project

---
+++ src/main/java/com/demo1/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.demo1.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserMapper extends BaseMapper {
+
+}
--- src/test/java/com/megumi/MegumiApplicationTests.java
+++ src/test/java/com/megumi/MegumiApplicationTests.java
@@ -8,6 +8,7 @@ class MegumiApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
